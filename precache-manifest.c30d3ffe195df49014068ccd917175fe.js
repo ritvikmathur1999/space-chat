2bcc92254652f46df31e37f430d3845f9ecabd6d
ritvikmathur1999 <ritvikmathur1999@gmail.com>
2021-07-23 00:07:45 +0530

    Updates

---
+++ precache-manifest.c30d3ffe195df49014068ccd917175fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64985d8307ed51b99b00bfc9986f7205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec192ca1f7ad1a9b704",
+    "url": "/static/css/main.655668af.chunk.css"
+  },
+  {
+    "revision": "0f7b6ff68c465af6415b",
+    "url": "/static/js/2.9627342a.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.9627342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec192ca1f7ad1a9b704",
+    "url": "/static/js/main.8ee69aac.chunk.js"
+  },
+  {
+    "revision": "5ead607fcb6a754c5038",
+    "url": "/static/js/runtime-main.59062f22.js"
+  },
+  {
+    "revision": "0c775419c40c32f8f57bd7084057a573",
+    "url": "/static/media/s.0c775419.png"
+  },
+  {
+    "revision": "a50f1b6cc90ce195ed76be8bcbfb54ca",
+    "url": "/static/media/s.a50f1b6c.gif"
+  }
+]);
